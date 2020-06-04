608b77b54e66a7336d74aecae52859a8501f3cc0
Nidhi Rastogi <nidhi.gupta@gmail.com>
2020-06-04 13:58:19 -0400

    Add files via upload

---
+++ vuldeepecker_predict.py
@@ -0,0 +1,96 @@
+"""
+Interface to VulDeePecker project
+"""
+import sys
+import os
+import pandas
+from clean_gadget import clean_gadget
+from vectorize_gadget import GadgetVectorizer
+from blstm import BLSTM
+
+"""
+Parses gadget file to find individual gadgets
+Yields each gadget as list of strings, where each element is code line
+Has to ignore first line of each gadget, which starts as integer+space
+At the end of each code gadget is binary value
+    This indicates whether or not there is vulnerability in that gadget
+"""
+def parse_file(filename):
+    with open(filename, "r", encoding="utf8") as file:
+        gadget = []
+        gadget_val = 0
+        for line in file:
+            stripped = line.strip()
+            if not stripped:
+                continue
+            if "-" * 33 in line and gadget: 
+                yield clean_gadget(gadget), gadget_val
+                gadget = []
+            elif stripped.split()[0].isdigit():
+                if gadget:
+                    # Code line could start with number (somehow)
+                    if stripped.isdigit():
+                        gadget_val = int(stripped)
+                    else:
+                        gadget.append(stripped)
+            else:
+                gadget.append(stripped)
+
+"""
+Uses gadget file parser to get gadgets and vulnerability indicators
+Assuming all gadgets can fit in memory, build list of gadget dictionaries
+    Dictionary contains gadgets and vulnerability indicator
+    Add each gadget to GadgetVectorizer
+Train GadgetVectorizer model, prepare for vectorization
+Loop again through list of gadgets
+    Vectorize each gadget and put vector into new list
+Convert list of dictionaries to dataframe when all gadgets are processed
+"""
+def get_vectors_df(filename, vector_length=100):
+    gadgets = []
+    count = 0
+    vectorizer = GadgetVectorizer(vector_length)
+    for gadget, val in parse_file(filename):
+        count += 1
+        print("Collecting gadgets...", count, end="\r")
+        vectorizer.add_gadget(gadget)
+        row = {"gadget" : gadget, "val" : val}
+        gadgets.append(row)
+    print('Found {} forward slices and {} backward slices'
+          .format(vectorizer.forward_slices, vectorizer.backward_slices))
+    print()
+    print("Training model...", end="\r")
+    vectorizer.train_model()
+    print()
+    vectors = []
+    count = 0
+    for gadget in gadgets:
+        count += 1
+        print("Processing gadgets...", count, end="\r")
+        vector = vectorizer.vectorize(gadget["gadget"])
+        row = {"vector" : vector, "val" : gadget["val"]}
+        vectors.append(row)
+    print()
+    df = pandas.DataFrame(vectors)
+    return df
+            
+"""
+Gets filename, either loads vector DataFrame if exists or creates it if not
+Instantiate neural network, pass data to it, train, test, print accuracy
+"""
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python vuldeepecker.py [data filename] [model file]")
+        exit()
+    filename = sys.argv[1]
+    modelpath = sys.argv[2]
+    parse_file(filename)
+    base = os.path.splitext(os.path.basename(filename))[0]
+    vector_length = 50
+    df = get_vectors_df(filename, vector_length)
+    blstm = BLSTM()
+    blstm.load(modelpath)
+    blstm.predict(df)
+
+if __name__ == "__main__":
+    main()
